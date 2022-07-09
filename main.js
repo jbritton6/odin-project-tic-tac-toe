424c60f934cd977514124259acf42816904f76d7
Jacob Britton <jacobbritton95@gmail.com>
2022-07-08 20:05:22 -0400

    Implement factory for board with skeleton functionality

--- main.js
+++ main.js
@@ -24,8 +24,41 @@ const Player = () => {
     };
 };
 
+const Board = () => {
+    const board = [];
+
+    const clearBoard = () => {
+        board.length = 0;
+        board[0] = [null, null, null];
+        board[1] = board[0];
+        board[2] = board[0];
+
+        return board;
+    };
+
+    const initalizeBoard = () => {
+        cells = document.querySelectorAll(".cell");
+        for (let cell of cells) {
+            cell.addEventListener('click', () => {
+                alert('hello');
+            });
+        };
+    };
+
+    return {
+        clearBoard,
+        initalizeBoard,
+    };
+};
+
+/*
 const jim = Player();
 jim.setPlayerName();
 console.log(jim.getPlayerName());
 jim.setPlayerToken("X");
 console.log(jim.getPlayerToken());
+*/
+
+const board = Board();
+board.initalizeBoard();
+console.log(board.clearBoard());
