1f33cb977c5ad45b1cd9d83e420bf05792e13134
Jacob Britton <jacobbritton95@gmail.com>
2022-07-08 22:32:29 -0400

    Add additional board functions
    Add ability to set new board, display board, event listeners
    on each cell and check for a winner.

--- main.js
+++ main.js
@@ -24,30 +24,92 @@ const Player = () => {
     };
 };
 
+
 const Board = () => {
-    const board = [];
+    let board;
+    let activePlayerToken = 0;
 
     const clearBoard = () => {
-        board.length = 0;
-        board[0] = [null, null, null];
-        board[1] = board[0];
-        board[2] = board[0];
-
+        board = Array(3).fill(null).map(() => Array(3).fill(null)); 
         return board;
     };
+
+    const logBoard = () => {
+        return board;
+    }
 
     const initalizeBoard = () => {
         cells = document.querySelectorAll(".cell");
         for (let cell of cells) {
             cell.addEventListener('click', () => {
-                alert('hello');
+                const classes = cell.className
+                const row = classes.match(/(?<=row-)\d/)[0];
+                const col = classes.match(/(?<=row-\d-col-)\d/)[0];
+
+                console.log(row, col);
+                cell.textContent = activePlayerToken;
+                board[row][col] = activePlayerToken;
             });
         };
     };
 
+    const checkWinner = () => {
+        if (
+            (board[0][0] === board[0][1]) && 
+            (board[0][1] === board[0][2])
+        ) {
+            return board[0][0];
+
+        } else if (
+            (board[1][0] === board[1][1]) && 
+            (board[1][1] === board[1][2])
+        ) {
+            return board[1][0];
+
+        } else if (
+            (board[2][0] === board[2][1]) && 
+            (board[2][1] === board[2][2])
+        ) {
+            return board[2][0];
+
+        } else if (
+            (board[0][0] === board[1][0]) && 
+            (board[1][0] === board[2][0])
+        ) {
+            return board[0][0];
+
+        } else if (
+            (board[0][1] === board[1][1]) && 
+            (board[1][1] === board[2][1])
+        ) {
+            return board[0][1];
+
+        } else if (
+            (board[0][2] === board[1][2]) && 
+            (board[1][2] === board[2][2])
+        ) {
+            return board[0][2];
+
+        } else if (
+            (board[0][0] === board[1][1]) && 
+            (board[1][1] === board[2][2])
+        ) {
+            return board[0][0];
+
+        } else if (
+            (board[2][0] === board[1][1]) && 
+            (board[1][1] === board[0][2])
+        ) {
+            return board[2][0];
+        }
+    };
+
     return {
+        board,
+        logBoard,
         clearBoard,
         initalizeBoard,
+        checkWinner,
     };
 };
 
@@ -60,5 +122,7 @@ console.log(jim.getPlayerToken());
 */
 
 const board = Board();
+board.clearBoard();
 board.initalizeBoard();
-console.log(board.clearBoard());
+console.log(board.checkWinner());
+//console.log(board.clearBoard());
