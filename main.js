521205ce5f6eed07834c6743845500b3f7191607
Jacob Britton <jacobbritton95@gmail.com>
2022-07-09 20:18:35 -0400

    Add popup for winner

--- main.js
+++ main.js
@@ -57,6 +57,10 @@ const Board = (player1, player2) => {
         return false;
     };
 
+    const displayWinner = () => {
+        alert(activePlayer.getPlayerName());
+    };
+
     const setCellValue = (cell) => {
         [row, col] = getCellPosition(cell);
 
@@ -65,6 +69,7 @@ const Board = (player1, player2) => {
             cell.textContent = activePlayer.getPlayerToken();
             board[row][col] = activePlayer.getPlayerToken();
 
+            checkWinner();
             switchActivePlayer(player1, player2);
         }
     };
@@ -89,50 +94,66 @@ const Board = (player1, player2) => {
     const checkWinner = () => {
         if (
             (board[0][0] === board[0][1]) && 
-            (board[0][1] === board[0][2])
+            (board[0][1] === board[0][2]) &&
+            (board[0][0] !== null)
         ) {
+            displayWinner();
             return board[0][0];
 
         } else if (
             (board[1][0] === board[1][1]) && 
-            (board[1][1] === board[1][2])
+            (board[1][1] === board[1][2]) &&
+            (board[1][0] !== null)
         ) {
+            displayWinner();
             return board[1][0];
 
         } else if (
             (board[2][0] === board[2][1]) && 
-            (board[2][1] === board[2][2])
+            (board[2][1] === board[2][2]) &&
+            (board[2][1] !== null)
         ) {
+            displayWinner();
             return board[2][0];
 
         } else if (
             (board[0][0] === board[1][0]) && 
-            (board[1][0] === board[2][0])
+            (board[1][0] === board[2][0]) &&
+            (board[0][0] !== null)
         ) {
+            displayWinner();
             return board[0][0];
 
         } else if (
             (board[0][1] === board[1][1]) && 
-            (board[1][1] === board[2][1])
+            (board[1][1] === board[2][1]) &&
+            (board[0][1] !== null)
         ) {
+            displayWinner();
             return board[0][1];
 
         } else if (
             (board[0][2] === board[1][2]) && 
-            (board[1][2] === board[2][2])
+            (board[1][2] === board[2][2]) &&
+            (board[0][2] !== null)
         ) {
+            displayWinner();
             return board[0][2];
 
         } else if (
             (board[0][0] === board[1][1]) && 
-            (board[1][1] === board[2][2])
+            (board[1][1] === board[2][2]) &&
+            (board[0][0] !== null)
         ) {
+            displayWinner();
             return board[0][0];
 
         } else if (
             (board[2][0] === board[1][1]) && 
-            (board[1][1] === board[0][2])
+            (board[1][1] === board[0][2]) &&
+            (board[2][0] !== null)
         ) {
+            displayWinner();
             return board[2][0];
         }
     };
